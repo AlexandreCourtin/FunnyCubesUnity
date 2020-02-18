91eec60d47385549b24651a8017dce7502a932c4
acourtin <acourtin@student.42.fr>
2020-02-18 19:24:11 +0100

    new cool way to play ith cubes

--- Assets/Scripts/CuriousCube.cs
+++ Assets/Scripts/CuriousCube.cs
@@ -10,7 +10,10 @@ public class CuriousCube : MonoBehaviour
 	public float redIntensity = .5f;
 	public float greenIntensity = .7f;
 	public float blueIntensity = .1f;
+	public float mouseDiffScale;
+	public float mouseDiffColor;
 
+	public bool isClicking;
 	MouseController mouse;
 	Animator cubeAnimator;
 	Material cubeMat;
@@ -18,11 +21,14 @@ public class CuriousCube : MonoBehaviour
 	Vector3 newPos;
 
 	void Start() {
+		isClicking = false;
 		mouse = GameObject.Find("Camera").GetComponent<MouseController>();
 		cubeAnimator = GetComponent<Animator>();
 		cubeMat = transform.Find("Body").GetComponent<MeshRenderer>().material;
 		originalPos = transform.position;
 		newPos = transform.position;
+		mouseDiffScale = 0f;
+		mouseDiffColor = 0f;
 
 		// ASSIGN RANDOM COLOR
 		float randomTest = Random.Range(0f, 3f);
@@ -39,47 +45,41 @@ public class CuriousCube : MonoBehaviour
 			greenIntensity = .75f;
 			blueIntensity = .25f;
 		}
+	}
 
-		// StartCoroutine(changeNewPos());
+	void Update() {
+		isClicking = Input.GetMouseButton(0);
+
+		mouseDiff = (100f - (Mathf.Pow(Mathf.Abs(relativeMousePosition.x), 4f)
+			+ Mathf.Pow(Mathf.Abs(relativeMousePosition.y), 4f))) * .06f;
+		if (mouseDiff < 0f)
+			mouseDiff = 0f;
+
+		relativeMousePosition = new Vector2(mouse.mousePosition.x - transform.position.x,
+			mouse.mousePosition.y - transform.position.y);
 	}
 
 	void FixedUpdate() {
 		// ROTATION
-		relativeMousePosition = new Vector2(mouse.mousePosition.x - transform.position.x,
-			mouse.mousePosition.y - transform.position.y);
 		transform.eulerAngles = new Vector3((relativeMousePosition.y * 2), (-relativeMousePosition.x * 2), 0f);
 
 		// FACE ANIMATION
 		cubeAnimator.SetFloat("X", -relativeMousePosition.x);
 		cubeAnimator.SetFloat("Y", relativeMousePosition.y);
 
-
 		// SCALE
-		mouseDiff = (50f - (Mathf.Pow(Mathf.Abs(relativeMousePosition.x), 1.5f) + Mathf.Pow(Mathf.Abs(relativeMousePosition.y), 1.5f))) * .06f;
-		float mouseDiffScale = Mathf.Clamp(mouseDiff, .5f, 1.6f);
+		if (isClicking) mouseDiffScale += mouseDiff * 4f * Time.fixedDeltaTime;
+		mouseDiffScale -= .1f * Time.fixedDeltaTime;
+		mouseDiffScale = Mathf.Clamp(mouseDiffScale, .5f, 1.6f);
 		transform.localScale = new Vector3(mouseDiffScale, mouseDiffScale, mouseDiffScale);
 
 		// COLORS
-		float mouseDiffColor = Mathf.Clamp(mouseDiff, .2f, 1f);
+		if (isClicking) mouseDiffColor += mouseDiff * 4f * Time.fixedDeltaTime;
+		mouseDiffColor -= .1f * Time.fixedDeltaTime;
+		mouseDiffColor = Mathf.Clamp(mouseDiffColor, .2f, 1f);
 		Color newColor = new Color(mouseDiffColor * redIntensity, mouseDiffColor * greenIntensity, mouseDiffColor * blueIntensity, 1f);
 		cubeMat.SetColor("_Color", newColor);
 
-		// SHAKING
-		// transform.position = Vector3.Lerp(transform.position, newPos, speed * Time.fixedDeltaTime);
-		transform.position = originalPos + new Vector3(relativeMousePosition.x, relativeMousePosition.y, 0f)
-			* mouseDiff * .002f;
-	}
 
-	IEnumerator changeNewPos()
-	{
-		while (true)
-		{
-			// SHAKING
-			newPos = new Vector3(originalPos.x + Random.Range(-5f, 5f),
-				originalPos.y + Random.Range(-5f, 5f),
-				originalPos.z);
-
-			yield return new WaitForSeconds(.05f);
-		}
 	}
 }
--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class MouseController : MonoBehaviour
 {
+	public float reactionTime = 5f;
+
 	public Vector3 mousePosition;
 
-	public float reactionSpeed = 3f;
+	void Start() {
+		mousePosition = resetPos();
+	}
 
 	void Update() {
 		Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 		RaycastHit hit;
 		if (Physics.Raycast(mouseRay, out hit)) {
-			mousePosition = Vector2.Lerp(mousePosition, hit.point, reactionSpeed * Time.deltaTime);
+			mousePosition = Vector3.Lerp(mousePosition, hit.point, reactionTime * Time.deltaTime);
 		}
+	}
+
+	Vector3 resetPos() {
+		return new Vector3(0f, 0f, 0f);
 	}
 }
